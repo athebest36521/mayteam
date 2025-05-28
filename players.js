a82ea06c687b7fca8f8ffc37787409df9a780d0d
athebest36521 <526atiwari@frhsd.com>
2025-05-28 08:34:57 -0400

    Last Commit from school computer

--- players.js
+++ players.js
@@ -1,135 +1,135 @@
 let players = [
-    {
-        firstname: "Luka",
-        lastname: "Dončić",
-        age: 26,
-        position: "Forward-Guard",
-        jersey: 77,
-        picture: "imgs/2425_RosterPage_Player_LukaDoncic_3_1040x1210.avif",
-        
-},
-{
-        firstname: "Dorian",
-        lastname: "Finney-Smith",
-        age: 32,
-        position: "Forward",
-        jersey: 17,
-        picture: "imgs/2425_RosterPage_Player_DorianFinneySmith_1040x1210_2.webp",
-        
-},{
-        firstname: "Jordan",
-        lastname: "Goodwin",
-        age: 26,
-        position: "Guard",
-        jersey: 30,
-        picture: "imgs/2425_RosterPage_Player_JordanGoodwin_1040x1210.avif",
-        
-},{
-        firstname: "Rui",
-        lastname: "Hachimura",
-        age: 29,
-        position: "Forward",
-        jersey: 28,
-        picture: "imgs/2425_RosterPage_Player_RuiHachimura_1040x1210.avif",
-        
-},{
-        firstname: "Jaxson",
-        lastname: "Hayes",
-        age: 24,
-        position: "Center-Forward",
-        jersey: 11,
-        picture: "imgs/2425_RosterPage_Player_JaxonHayes_1040x1210-1.webp",
-        
-},{
-        firstname: "Bronny",
-        lastname: "James",
-        age: 20,
-        position: "Guard",
-        jersey: 9,
-        picture: "imgs/2425_RosterPage_Player_BronnyJames_1040x1210.avif",
-        
-},{
-        firstname: "LeBron",
-        lastname: "James",
-        age: 40,
-        position: "Forward",
-        jersey: 23,
-        picture: "imgs/2425_RosterPage_Player_LeBronJames_1040x1210.avif",
-        
-},{
-        firstname: "Dalton",
-        lastname: "Knecht",
-        age: 24,
-        position: "Forward",
-        jersey: 4,
-        picture: "imgs/2425_RosterPage_Player_DaltonKnecht_1040x1210.webp",
-        
-},{
-        firstname: "Alex",
-        lastname: "Len",
-        age: 31,
-        position: "Center",
-        jersey: 27,
-        picture: "imgs/2425_RosterPage_Player_AlexLen_2_1040x1210.avif",
-        
-},{
-        firstname: "Shake",
-        lastname: "Milton",
-        age: 28,
-        position: "Guard",
-        jersey: 20,
-        picture: "imgs/2425_RosterPage_Player_ShakeMilton_1040x1210_2.webp",
-        
-},{
-        firstname: "Markieff",
-        lastname: "Morris",
-        age: 35,
-        position: "Forward",
-        jersey: 88,
-        picture: "imgs/2425_RosterPage_Player_MarkieffMorris_2_1040x1210.webp",
-        
-},{
-        firstname: "Austin",
-        lastname: "Reaves",
-        age: 26,
-        position: "Guard",
-        jersey: 15,
-        picture: "imgs/2425_RosterPage_Player_AustinReaves_1040x1210.avif",
-        
-},{
-        firstname: "Jarred",
-        lastname: "Vanderbilt",
-        age: 24,
-        position: "Forward",
-        jersey: 2,
-        picture: "imgs/2425_RosterPage_Player_JarredVanderbilt_1040x1210.avif",
-        
-},{
-        firstname: "Gabe",
-        lastname: "Vincent",
-        age: 27,
-        position: "Guard",
-        jersey: 7,
-        picture: "imgs/2425_RosterPage_Player_GabeVincent_1040x1210.avif",
-        
-},{
-        firstname: "Trey",
-        lastname: "Jemison",
-        suffix: "III",
-        age: 25,
-        position: "Center",
-        jersey: 55,
-        picture: "imgs/2425_RosterPage_Player_TreyJeminson_1040x1210.avif",
-        
-},{
-        firstname: "Christian",
-        lastname: "Koloko",
-        age: 24,
-        position: "Center",
-        jersey: 10,
-        picture: "imgs/2425_RosterPage_Player_ChristianKoloko_1040x1210.webp",
-        
-}
+        {
+                firstname: "Luka",
+                lastname: "Dončić",
+                age: 26,
+                position: "Forward-Guard",
+                jersey: 77,
+                picture: "imgs/2425_RosterPage_Player_LukaDoncic_3_1040x1210.avif",
+
+        },
+        {
+                firstname: "Dorian",
+                lastname: "Finney-Smith",
+                age: 32,
+                position: "Forward",
+                jersey: 17,
+                picture: "imgs/2425_RosterPage_Player_DorianFinneySmith_1040x1210_2.webp",
+
+        }, {
+                firstname: "Jordan",
+                lastname: "Goodwin",
+                age: 26,
+                position: "Guard",
+                jersey: 30,
+                picture: "imgs/2425_RosterPage_Player_JordanGoodwin_1040x1210.avif",
+
+        }, {
+                firstname: "Rui",
+                lastname: "Hachimura",
+                age: 29,
+                position: "Forward",
+                jersey: 28,
+                picture: "imgs/2425_RosterPage_Player_RuiHachimura_1040x1210.avif",
+
+        }, {
+                firstname: "Jaxson",
+                lastname: "Hayes",
+                age: 24,
+                position: "Center-Forward",
+                jersey: 11,
+                picture: "imgs/2425_RosterPage_Player_JaxonHayes_1040x1210-1.webp",
+
+        }, {
+                firstname: "Bronny",
+                lastname: "James",
+                age: 20,
+                position: "Guard",
+                jersey: 9,
+                picture: "imgs/2425_RosterPage_Player_BronnyJames_1040x1210.avif",
+
+        }, {
+                firstname: "LeBron",
+                lastname: "James",
+                age: 40,
+                position: "Forward",
+                jersey: 23,
+                picture: "imgs/2425_RosterPage_Player_LeBronJames_1040x1210.avif",
+
+        }, {
+                firstname: "Dalton",
+                lastname: "Knecht",
+                age: 24,
+                position: "Forward",
+                jersey: 4,
+                picture: "imgs/2425_RosterPage_Player_DaltonKnecht_1040x1210.webp",
+
+        }, {
+                firstname: "Alex",
+                lastname: "Len",
+                age: 31,
+                position: "Center",
+                jersey: 27,
+                picture: "imgs/2425_RosterPage_Player_AlexLen_2_1040x1210.avif",
+
+        }, {
+                firstname: "Shake",
+                lastname: "Milton",
+                age: 28,
+                position: "Guard",
+                jersey: 20,
+                picture: "imgs/2425_RosterPage_Player_ShakeMilton_1040x1210_2.webp",
+
+        }, {
+                firstname: "Markieff",
+                lastname: "Morris",
+                age: 35,
+                position: "Forward",
+                jersey: 88,
+                picture: "imgs/2425_RosterPage_Player_MarkieffMorris_2_1040x1210.webp",
+
+        }, {
+                firstname: "Austin",
+                lastname: "Reaves",
+                age: 26,
+                position: "Guard",
+                jersey: 15,
+                picture: "imgs/2425_RosterPage_Player_AustinReaves_1040x1210.avif",
+
+        }, {
+                firstname: "Jarred",
+                lastname: "Vanderbilt",
+                age: 24,
+                position: "Forward",
+                jersey: 2,
+                picture: "imgs/2425_RosterPage_Player_JarredVanderbilt_1040x1210.avif",
+
+        }, {
+                firstname: "Gabe",
+                lastname: "Vincent",
+                age: 27,
+                position: "Guard",
+                jersey: 7,
+                picture: "imgs/2425_RosterPage_Player_GabeVincent_1040x1210.avif",
+
+        }, {
+                firstname: "Trey",
+                lastname: "Jemison",
+                suffix: "III",
+                age: 25,
+                position: "Center",
+                jersey: 55,
+                picture: "imgs/2425_RosterPage_Player_TreyJeminson_1040x1210.avif",
+
+        }, {
+                firstname: "Christian",
+                lastname: "Koloko",
+                age: 24,
+                position: "Center",
+                jersey: 10,
+                picture: "imgs/2425_RosterPage_Player_ChristianKoloko_1040x1210.webp",
+
+        }
 ]
 
 
--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const toggleDarkMode = () => {
   document.body.classList.toggle("dark-mode");
   document.querySelectorAll('button').forEach(button => {
-      button.classList.toggle("dark-mode");
+    button.classList.toggle("dark-mode");
   });
   document.querySelector('header').classList.toggle("dark-mode");
   document.querySelector('footer').classList.toggle("dark-mode");
   document.querySelectorAll('.card').forEach(button => {
     button.classList.toggle("dark-mode");
-});
+  });
 };
